95fa93498925a6c5c0637763c9c22ec169ebd59f
Mo2men7 <momen.helmy.m@gmail.com>
2024-07-08 03:04:07 +0300

    Defining some prop types

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,6 +11,12 @@ const starContainerStyle = {
   display: "flex",
 };
 
+StarRating.propTypes = {
+  // maxRating: PropTypes.number.isRequired,
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+};
 export default function StarRating({
   maxRating = 5,
   color = "#fcc419",
